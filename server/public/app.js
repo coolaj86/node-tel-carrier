02a8b1115d6fdb42fcaf9ff9712b292c48bef232
AJ ONeal <coolaj86@gmail.com>
2014-02-17 21:35:46 +0000

    allow bulk lookup via GET

--- server/public/app.js
+++ server/public/app.js
@@ -4,7 +4,34 @@ $(function () {
   $('body').on('submit', '.js-form', function (ev) {
     ev.preventDefault();
     ev.stopPropagation();
-    $.get('/lookup?number=' + $('.js-number').val(), function (data) {
+
+    var text = $('textarea.js-numbers').val()
+      , lines
+      , numbers = []
+      , formatted = []
+      ;
+
+    console.log('text', text);
+    lines = text.split(/\s*[,\n]+\s*/);
+    console.log('lines.length', lines.length);
+    lines.forEach(function (line) {
+      var parts = /\s*(?=\+?\s*1)?[\-\.\(\s]*(\d{3})[\-\.\)\s]*(\d{3})[\-\.\s]*(\d{4})\s*(?=\D|$)/.exec(line)
+        ;
+
+      if (!parts) {
+        console.warn(line);
+        return;
+      }
+
+      numbers.push(parts[1].toString() + parts[2] + parts[3]);
+      formatted.push('(' + parts[1] + ') ' + parts[2] + '-' + parts[3]);
+    });
+    console.log('numbers.length', numbers.length);
+    
+    $('.js-numbers').val(formatted.join('\n'));
+
+    $.get('/lookup?numbers=' + numbers.join(','), function (data) {
+      console.log('data.length', data.length);
       $('.js-result').text(JSON.stringify(data, null, '  '));
     });
   });
